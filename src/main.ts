cb1348ad549d29a1761723f63061227972c09f8b
Otavio Herzog <otaviohz@proton.me>
2026-03-06 18:51:01 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,16 @@
 import * as Tone from "tone";
-import { Scale } from "tonal";
 
-const button = document.getElementById("play")!;
 
-button.addEventListener("click", async () => {
-  await Tone.start(); // Required on user gesture
-  console.log("Audio ready!");
+const buttonPlay = document.getElementById("play")!;
+const buttonPause = document.getElementById("pause")!;
 
-  const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth().toDestination();
 
-  // Choose any scale
-  const notes = Scale.get("A major").notes.map(n => n + "4");
-  console.log("Playing:", notes);
 
-  // Play each note in sequence
-  notes.forEach((note, i) => {
-    synth.triggerAttackRelease(
-      note,
-      "32n",
-      Tone.now() + i * .5,
-    );
-  });
+buttonPlay.addEventListener("click", async () => {
+  console.log("pressed play")
+  synth.triggerAttackRelease("c4", "4n")
+});
+buttonPause.addEventListener("click", async () => {
+  console.log("pressed pause")
 });
